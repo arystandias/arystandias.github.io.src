19542c09d143710b62c5ac09579e94e0313e33dc
Dias <arystan.dias.kz@gmail.com>
2022-06-09 20:55:26 +0500

    feat: Добавить [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,43 +1,41 @@
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
 
-import JavascriptIntroduction from '../../posts/javascript/introduction.md';
-import TypescriptIntroduction from '../../posts/typescript/introduction.md';
+import JavascriptIntroduction from "../../posts/javascript/introduction.md";
+import TypescriptIntroduction from "../../posts/typescript/introduction.md";
 
-import Array from '../../posts/javascript/Array.md';
-import ReactMarkdown from 'react-markdown'
-import { useEffect, useRef, useState } from 'react';
-import { ParsedUrlQuery } from 'querystring';
-import Category from '../../components/Category/Category';
+import Array from "../../posts/javascript/Array.md";
+import ReactMarkdown from "react-markdown";
+import { useEffect, useRef, useState } from "react";
+import { ParsedUrlQuery } from "querystring";
+import Category from "../../components/Category/Category";
 
 export default function Slug() {
-    const router = useRouter();
-    const [markdown, setMarkdown] = useState('');
-    
-    //const [query, setQuery] = useState<ParsedUrlQuery>(router.query);
-    
-    //const ref = useRef();
+  const router = useRouter();
+  const [markdown, setMarkdown] = useState("");
 
-    //var items: String[] = [];
+  //const [query, setQuery] = useState<ParsedUrlQuery>(router.query);
 
+  //const ref = useRef();
 
-    // useEffect(() => {
-    //   if (router.isReady) {
-    //     console.log('isReady');
-    //     console.log(router.query);
+  //var items: String[] = [];
 
-    //     if (router.query && router.query.slug === 'javascript') {
-    //       setMarkdown(JavascriptIntroduction);
-    //     }
-    //     if (router.query && router.query.slug === 'typescript') {
-    //       setMarkdown(TypescriptIntroduction);
-    //     }
+  // useEffect(() => {
+  //   if (router.isReady) {
+  //     console.log('isReady');
+  //     console.log(router.query);
 
+  //     if (router.query && router.query.slug === 'javascript') {
+  //       setMarkdown(JavascriptIntroduction);
+  //     }
+  //     if (router.query && router.query.slug === 'typescript') {
+  //       setMarkdown(TypescriptIntroduction);
+  //     }
 
-    //     console.log('slug: '+router.query.slug);
-    //   }
-    // }, [router.isReady]);                                                                                                                                           ]);
+  //     console.log('slug: '+router.query.slug);
+  //   }
+  // }, [router.isReady]);                                                                                                                                           ]);
 
-    //return <ReactMarkdown>{markdown}</ReactMarkdown>
-    console.log('OK1');
-    return <Category/>;
+  //return <ReactMarkdown>{markdown}</ReactMarkdown>
+  console.log("OK1");
+  return <Category />;
 }
