7e85ddd2039647d9f20d2576aff75b21065c3eae
Dias <arystan.dias.kz@gmail.com>
2022-06-09 20:41:24 +0500

    feat: Добавить компонент Хлебных крошек

---
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import Breadcrumbs from '@mui/material/Breadcrumbs';
+import HomeIcon from '@mui/icons-material/Home';
+import WhatshotIcon from '@mui/icons-material/Whatshot';
+import GrainIcon from '@mui/icons-material/Grain';
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+
+
+
+function IconBreadcrumbs():JSX.Element {
+  const router = useRouter();
+  const { slug } = router.query;
+  console.log(slug);
+
+  //function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+  //  event.preventDefault();
+  //  //console.info(HTMLDivElement.);
+  //  const router = useRouter();
+  //  router.push('/contacts');
+  //}
+  // onClick={(e) => router.push(slug?.toString())}
+  return (
+    <div role="presentation">
+      <Breadcrumbs aria-label="breadcrumb">
+        
+        <Link
+          color="inherit"
+          href={'/'}
+          style={{cursor: 'pointer'}}
+        >
+          <a><HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" /> Главная</a>
+        </Link>
+        
+        <Typography
+          sx={{ display: 'flex', alignItems: 'center' }}
+          color="text.primary"
+        >
+          {slug}
+        </Typography>
+        
+      </Breadcrumbs>
+    </div>
+  );
+}
+
+export default IconBreadcrumbs;
