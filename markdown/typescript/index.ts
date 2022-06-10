7388b2ba0efadc8c6a7ee9f9bae99bc0211571f4
Dias <arystan.dias.kz@gmail.com>
2022-06-11 02:00:03 +0500

    feat: Доработка маркдауна

---
+++ markdown/typescript/index.ts
@@ -0,0 +1,31 @@
+
+
+export const data = [
+   {
+      title: 'Асинхронный Typescript',
+      description: 'Обсуждение асинхронного Typescript: почему это так важно, как эта возможность языка может использована для обработки потенциальных блокирующих операций, как, например получение данных с сервера.',
+      image: '/typescript/logo.png',
+      created_date: '01.012022',
+      author: 'shandyrov askar',
+      post: '/posts/typescript/intro-1',
+      url: 'intro-1',   
+   },
+   {
+      title: 'Введение в объекты Typescript',
+      description: 'Объектно-ориентированная природа JavaScript важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/typescript/logo.png',
+      created_date: '02.012022',
+      author: 'ahandyrov askar',
+      post: '/posts/typescript/intro-2',
+      url: 'intro-2',
+   },
+   {
+      title: 'Введение в объекты Typescript 2',
+      description: 'Объектно-ориентированная природа JavaScript важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/typescript/logo.png',
+      created_date: '02.012022',
+      author: 'ahandyrov askar',
+      post: '/posts/typescript/intro-2',
+      url: 'intro-2',
+   },
+]; 
