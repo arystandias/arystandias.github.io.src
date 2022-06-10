e456f41687937a3e3f7f208c7955a6f6f72b27a8
Dias <arystan.dias.kz@gmail.com>
2022-06-10 23:37:42 +0500

    feat: Доработка index.ts

---
+++ markdown/javascript/index.ts
@@ -0,0 +1,22 @@
+
+
+export const data = [
+   {
+      title: 'Асинхронный JavaScript',
+      description: 'Обсуждение асинхронного JavaScript: почему это так важно, как эта возможность языка может использована для обработки потенциальных блокирующих операций, как, например получение данных с сервера.',
+      image: '/javascript/logo.png',
+      url: 'intro-1',
+      created_date: '01.012022',
+      author: 'shandyrov askar',
+      post: '/posts/javascript/intro-1'   
+   },
+   {
+      title: 'Введение в объекты JavaScript',
+      description: 'Объектно-ориентированная природа JavaScript важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/javascript/logo.png',
+      url: 'intro-2',
+      created_date: '02.012022',
+      author: 'ahandyrov askar',
+      post: '/posts/javascript/intro-2'
+   },
+]; 
