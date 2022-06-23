ecde16800852b91b24756d7521de0fcf280a9b05
Dias <arystan.dias.kz@gmail.com>
2022-06-23 23:59:34 +0500

    feat: Добавить markdown/categories/index.ts

---
+++ markdown/categories/index.ts
@@ -0,0 +1,25 @@
+
+
+export const data = [
+   {
+      title: 'JavaScript',
+      description: '',
+      image: '/posts/javascript/logo.png',
+      created_date: '03.10.2021',
+      author: 'shandyrov askar',   
+   },
+   {
+      title: 'TypeScript',
+      description: '',
+      image: '/posts/typescript/logo.png',
+      created_date: '02.01.2022',
+      author: 'shandyrov askar',
+   },
+   {
+      title: 'Docker',
+      description: '',
+      image: '/posts/docker/logo.png',
+      created_date: '02.01.2022',
+      author: 'shandyrov askar',
+   }
+]; 
