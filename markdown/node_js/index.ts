b2d3f8686accbabda4768f5640138e3ef39e9712
arystanalan <arystandias.github.io@gmail.com>
2022-06-24 22:22:44 +0500

    feat: Add react makdown

---
+++ markdown/node_js/index.ts
@@ -0,0 +1,40 @@
+
+
+export const data = [
+   {
+      title: 'Введение в node js',
+      description: 'node js — свободная реляционная система управления базами данных. Разработку и поддержку MySQL осуществляет корпорация Oracle, получившая права на торговую марку вместе с поглощённой Sun Microsystems, которая ранее приобрела шведскую компанию php AB.',
+      image: '/posts/node_js/logo.png',
+      created_date: '24.04.2022',
+      author: 'shandyrov askar',
+      post: '/posts/redis/intro-1',
+      url: 'intro-1',   
+   },
+   {
+      title: 'Введение в объекты node_js 2',
+      description: 'Объектно-ориентированная природа react важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/posts/node_js/logo.png',
+      created_date: '02.01.2022',
+      author: 'ahandyrov askar',
+      post: '/posts/node_js/intro-2',
+      url: 'intro-2',
+   },
+      {
+      title: 'Введение в объекты node_js 3',
+      description: 'Объектно-ориентированная природа react важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/posts/node_js/logo.png',
+      created_date: '02.01.2022',
+      author: 'ahandyrov askar',
+      post: '/posts/node_js/intro-2',
+      url: 'intro-2',
+   },
+      {
+      title: 'Введение в объекты node_js 4',
+      description: 'Объектно-ориентированная природа react важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/posts/node_js/logo.png',
+      created_date: '02.01.2022',
+      author: 'ahandyrov askar',
+      post: '/posts/node_js/intro-2',
+      url: 'intro-2',
+   },
+]; 
---
+++ markdown/redis/index.ts
@@ -0,0 +1,40 @@
+
+
+export const data = [
+   {
+      title: 'Введение в redis',
+      description: 'redis — свободная реляционная система управления базами данных. Разработку и поддержку MySQL осуществляет корпорация Oracle, получившая права на торговую марку вместе с поглощённой Sun Microsystems, которая ранее приобрела шведскую компанию php AB.',
+      image: '/posts/redis/logo.png',
+      created_date: '24.04.2022',
+      author: 'shandyrov askar',
+      post: '/posts/redis/intro-1',
+      url: 'intro-1',   
+   },
+   {
+      title: 'Введение в объекты redis 2',
+      description: 'Объектно-ориентированная природа react важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/posts/redis/logo.png',
+      created_date: '02.01.2022',
+      author: 'ahandyrov askar',
+      post: '/posts/redis/intro-2',
+      url: 'intro-2',
+   },
+      {
+      title: 'Введение в объекты redis 3',
+      description: 'Объектно-ориентированная природа react важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/posts/redis/logo.png',
+      created_date: '02.01.2022',
+      author: 'ahandyrov askar',
+      post: '/posts/redis/intro-2',
+      url: 'intro-2',
+   },
+      {
+      title: 'Введение в объекты redis 4',
+      description: 'Объектно-ориентированная природа react важна для понимания, если вы хотите углубить знание языка и писать более эффективный код. Поэтому мы подготовили модуль, который поможет вам в этом.',
+      image: '/posts/redis/logo.png',
+      created_date: '02.01.2022',
+      author: 'ahandyrov askar',
+      post: '/posts/redis/intro-2',
+      url: 'intro-2',
+   },
+]; 
