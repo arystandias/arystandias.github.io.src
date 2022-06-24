de003cd789ec5c349166092b4b7a1a82c07bd6d6
arystanalan <arystandias.github.io@gmail.com>
2022-06-25 00:27:30 +0500

    feat: Доработка...

---
+++ markdown/flutter/index.ts
@@ -0,0 +1,14 @@
+
+
+export const data = [
+   {
+      title: 'Асинхронный Flutter 1',
+      description: 'Обсуждение асинхронного Flutter: почему это так важно, как эта возможность языка может использована для обработки потенциальных блокирующих операций, как, например получение данных с сервера.',
+      image: '/posts/flutter/logo.png',
+      created_date: '03.10.2021',
+      author: 'shandyrov askar',
+      post: '/posts/flutter/intro-1',
+      url: 'intro-1',   
+   },
+
+]; 
