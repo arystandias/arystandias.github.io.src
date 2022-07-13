835f858d1d472f73fb05bd7ab77067eaf5d733fc
arystanalan <arystandias.github.io@gmail.com>
2022-07-13 17:11:37 +0500

    feat:  Доработка TabPanel

--- components/TabPanel/TabPanel.tsx
+++ components/TabPanel/TabPanel.tsx
@@ -4,6 +4,8 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Contacts from "../TabPanel/Contacts";
+import { Octokit } from "octokit";
+import Head from "next/head";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -13,6 +15,19 @@ interface TabPanelProps {
 
 function TabPanel(props: TabPanelProps) {
   const { children, value, index, ...other } = props;
+
+  // React.useEffect(() => {
+  //   //fetch("https://api.github.com/users/arystandias")
+  //   //fetch("https://api.github.com/gists/public")
+  //   fetch("https://api.github.com/users/arystandias/gists")
+  //     .then((response) => response.json())
+  //     .then((data) => JSON.stringify(data))
+  //     .then((objects) => {
+  //       //console.log("Objects:");
+  //       //console.log(objects);
+  //       //setGistItems(JSON.parse(objects));
+  //     });
+  // }, []);
 
   return (
     <div
@@ -40,46 +55,81 @@ function a11yProps(index: number) {
 
 export default function BasicTabs() {
   const [value, setValue] = React.useState(0);
+  const [firstTab, setFirstTab] = React.useState("");
+  const [secondTab, setSecondTab] = React.useState();
+  const [thirdTab, setThirdTab] = React.useState();
+
+  React.useEffect(() => {
+    if (!firstTab) {
+      const octokit = new Octokit({
+        auth: "personal-access-token123",
+      });
+
+      console.log("[ok]");
+      // fetch("https://api.github.com/users/arystandias/")
+      //   .then((response) => response.json())
+      //   .then((data) => JSON.stringify(data))
+      //   .then((objects) => {
+      //     //console.log("Objects:");
+      //     //console.log(objects);
+      //     //setGistItems(JSON.parse(objects));
+
+      //     console.log(objects);
+      //   });
+
+      // fetch("https://api.github.com/users/arystandias/").then((response) =>
+      //   console.log(response)
+      // );
+
+      fetch("https://api.github.com/users/arystandias")
+        .then((response) => response.json())
+        .then((response) => setFirstTab(response.bio)); //console.log(response)
+    }
+  }, []);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
 
   return (
-    <Box
-      sx={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        paddingLeft: 2,
-        paddingTop: 0,
-      }}
-    >
-      <Box sx={{ borderBottom: 1, borderColor: "divider", paddingLeft: 2 }}>
-        <Tabs value={value} onChange={handleChange} aria-label="example">
-          <Tab label="Обо мне" {...a11yProps(0)} />
-          <Tab label="Проекты" {...a11yProps(1)} />
-          <Tab label="Контакты" {...a11yProps(0)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </TabPanel>
-      <TabPanel value={value} index={1}>
+    <>
+      <Head>
+        <title>Арыстан Диас</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Моя сайт о прогроммировании" />
+        <meta
+          name="keywords"
+          content="Арыстан Диас, Arystan Dias, Веб разработчик Актау, Веб разработчик Алматы, Roblox разработчик, Шандыров Аскар, Казахстан Актау, Flatter, React, NextJS"
+        />
+      </Head>
+
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          paddingLeft: 2,
+          paddingTop: 0,
+        }}
+      >
+        <Box sx={{ borderBottom: 1, borderColor: "divider", paddingLeft: 2 }}>
+          <Tabs value={value} onChange={handleChange} aria-label="example">
+            <Tab label="Обо мне" {...a11yProps(0)} />
+            {/* <Tab label="Проекты" {...a11yProps(1)} /> */}
+            <Tab label="Контакты" {...a11yProps(0)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          {firstTab}
+        </TabPanel>
+        {/* <TabPanel value={value} index={1}>
         Проекты
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <Contacts />
-      </TabPanel>
-    </Box>
+      </TabPanel> */}
+        <TabPanel value={value} index={1}>
+          <Contacts />
+        </TabPanel>
+      </Box>
+    </>
   );
 }
